b5a7fe23ccfa0908227ee6beb0d7734106e29d63
finabluma <oscar.francino@finabluma.com>
2021-01-29 19:05:39 +0100

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
